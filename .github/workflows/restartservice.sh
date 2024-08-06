811e5fbf2494c898b6b76d06bd961f00e6a6bbe7
Lily Cholmondeley <lily.cholmo1@gmail.com>
2024-08-06 03:14:45 +0100

    Please for the love of god work now

--- .github/workflows/restartservice.sh
+++ .github/workflows/restartservice.sh
@@ -6,6 +6,7 @@ pm2 save
 rm -rf ~/apps/soupbot
 mkdir ~/apps/soupbot
 cp -r ~/actions-runner/_work/soup-bot/soup-bot/* ~/apps/soupbot
+cp ~/soup-bot-main/.env ~/apps/soupbot
 cd ~/apps/soupbot
 pm2 start index.js --name soupbot
 pm2 save

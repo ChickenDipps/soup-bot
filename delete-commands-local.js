e75724a19bcc80810bef911e7586c279987175dd
Lily Cholmondeley <lily.cholmo1@gmail.com>
2024-10-01 11:51:25 +0100

    Added delete command functions

---
+++ delete-commands-local.js
@@ -0,0 +1,11 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+const token = process.env.TOKEN;
+
+const rest = new REST().setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+  .then(() => console.log('Successfully deleted all guild commands.'))
+  .catch(console.error);

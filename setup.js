02df1f9e22100d6fd468741cfc7769425039c490
Lily Cholmondeley <lily.cholmo1@gmail.com>
2025-06-08 22:56:48 +0100

    Add setup file

---
+++ setup.js
@@ -0,0 +1,11 @@
+const open = require('sqlite');
+const sqlite3 = require('sqlite3');
+
+export async function setup() {
+  const db = await open({
+    filename: './database.db',
+    driver: sqlite3.Database
+  });
+  await db.migrate({ migrationpath: './migrations' });
+  await db.close();
+}
